fec13130f0efac1c19d1a9c2ad11849db46548e6
yangoogle <yang657850144@gmail.com>
2019-03-29 18:10:03 +0800

    fix: 修复 vue-template-compiler 未引用问题

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+import Mock from 'mockjs'
+export default {
+  name: 'app',
+  mounted () {
+    const data = Mock.mock({
+      'list|1-10': [{
+        'id|+1': 1
+      }]
+    })
+    console.log('data', JSON.stringify(data))
+  }
+}
+</script>
